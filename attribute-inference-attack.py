30051d87e7da5233286b5cfb10aebabacf3af7c5
Pilladian <ph-zimm-acc@pm.me>
2021-05-19 00:06:53 +0200

    test model.pt

--- attribute-inference-attack.py
+++ attribute-inference-attack.py
@@ -3,5 +3,23 @@
 
 import UTKFace
 import Target
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 
-target = Target.Target(device='cuda', train=True, ds_root='UTKFace')
+
+transform = transforms.Compose(
+    [ transforms.Resize((356, 356)),
+      transforms.RandomCrop((299, 299)),
+      transforms.ToTensor(),
+      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+test_set = UTKFace.UTKFace(root='UTKFace', test=True, transform=transform)
+test_loader = DataLoader(dataset=test_set,
+                         shuffle=True,
+                         batch_size=32,
+                         num_workers=1)
+
+
+target = Target.Target()
+target.evaluate_model(target.model, test_loader)
