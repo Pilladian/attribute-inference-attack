b07eec2521502e009efdd58329e20df58259b0ba
Pilladian <ph-zimm-acc@pm.me>
2021-05-19 08:33:25 +0200

    gender -> age

--- UTKFace/create_csv.py
+++ UTKFace/create_csv.py
@@ -6,17 +6,23 @@ import os
 
 
 def create_csv(root):
-    data = pandas.DataFrame(columns=["img_file", "gender"])
+    data = pandas.DataFrame(columns=["img_file", "age"])
     data["img_file"] = os.listdir(root)
+
+    try:
+        os.system(f'rm {root}/data.csv')
+    except:
+        pass
 
     for idx, i in enumerate(os.listdir(root)):
         l = i.split('_')
         # female
-        if l[1] == '1':
-            data["gender"][idx] = 1
-        # male
-        elif l[1] == '0':
-            data["gender"][idx] = 0
+        # if l[1] == '1':
+        #     data["gender"][idx] = 1
+        # # male
+        # elif l[1] == '0':
+        #     data["gender"][idx] = 0
+        data["age"][idx] = int(l[0])
 
     data.to_csv(f"{root}data.csv", index=False, header=True)
 
